d0adccde243e30fe6d332507d8ae3b74efd9f972
Kevin Bridges <kevin@wiiBridges.com>
2021-02-28 12:24:12 -0600

    consolidate top-level NavBar divs

--- src/layout/NavBar.svelte
+++ src/layout/NavBar.svelte
@@ -26,106 +26,104 @@
            ALLOWING our NavBar to always be visible at the top of our app
  -->
 <nav class="flex-none bg-primaryLight">
-  <div class="max-w-7xl mx-auto px-3">
-    <div class="flex flex-wrap justify-between py-2.5">
+  <div class="flex flex-wrap justify-between px-3 py-2.5">
 
-      <!-- Left Section of NavBar -->
-      <div class="flex">
-        <div class="ml-1 flex items-center space-x-2">
-          <Icon name="menu"
-                title="Toggle SideBar"
-                class="PRIMARY-COLOR text-primary
-                       HOVER-COLOR   hover:text-primaryDark
-                       CURSOR        select-none cursor-pointer"
-                on:click={sideBar.toggle}/>
+    <!-- Left Section of NavBar -->
+    <div class="flex">
+      <div class="ml-1 flex items-center space-x-2">
+        <Icon name="menu"
+              title="Toggle SideBar"
+              class="PRIMARY-COLOR text-primary
+                     HOVER-COLOR   hover:text-primaryDark
+                     CURSOR        select-none cursor-pointer"
+              on:click={sideBar.toggle}/>
 
-          <!-- Selected Sample -->
-          <span class="SELECTED-COLOR  bg-primary text-onDark
-                       CURSOR          select-none cursor-pointer
-                       OTHER           px-3 py-2 rounded-md text-sm font-medium">
-            Dashboard
-          </span>
+        <!-- Selected Sample -->
+        <span class="SELECTED-COLOR  bg-primary text-onDark
+                     CURSOR          select-none cursor-pointer
+                     OTHER           px-3 py-2 rounded-md text-sm font-medium">
+          Dashboard
+        </span>
 
-          <!-- Non-Selected Samples -->
-          <span class="NON-SELECTED-COLOR text-onLight
-                       HOVER              hover:bg-primary hover:text-onDark
-                       CURSOR          select-none cursor-pointer
-                       OTHER              px-3 py-2 rounded-md text-sm font-medium">
-            Team
-          </span>
-          <span class="NON-SELECTED-COLOR text-onLight
-                       HOVER              hover:bg-primary hover:text-onDark
-                       CURSOR          select-none cursor-pointer
-                       OTHER              px-3 py-2 rounded-md text-sm font-medium">
-            Projects
-          </span>
-        </div>
-      </div>  <!-- end of ... Left Section of NavBar -->
+        <!-- Non-Selected Samples -->
+        <span class="NON-SELECTED-COLOR text-onLight
+                     HOVER              hover:bg-primary hover:text-onDark
+                     CURSOR          select-none cursor-pointer
+                     OTHER              px-3 py-2 rounded-md text-sm font-medium">
+          Team
+        </span>
+        <span class="NON-SELECTED-COLOR text-onLight
+                     HOVER              hover:bg-primary hover:text-onDark
+                     CURSOR          select-none cursor-pointer
+                     OTHER              px-3 py-2 rounded-md text-sm font-medium">
+          Projects
+        </span>
+      </div>
+    </div>  <!-- end of ... Left Section of NavBar -->
 
-      <!-- Right Section of NavBar -->
-      <div class="flex items-center">
-        <div class="ml-4 flex-shrink-0 flex items-center">
+    <!-- Right Section of NavBar -->
+    <div class="flex items-center">
+      <div class="ml-4 flex-shrink-0 flex items-center">
 
-          <span class="NON-SELECTED-COLOR text-onLight
-                       HOVER              hover:bg-primary hover:text-onDark
-                       CURSOR             select-none cursor-pointer
-                       OTHER              mr-1 px-3 py-2 rounded-md text-sm font-medium"
-                title="Next Theme"
-                on:click={() => theme = DCT.activateNextTheme()}>
-            {theme}
-          </span>
+        <span class="NON-SELECTED-COLOR text-onLight
+                     HOVER              hover:bg-primary hover:text-onDark
+                     CURSOR             select-none cursor-pointer
+                     OTHER              mr-1 px-3 py-2 rounded-md text-sm font-medium"
+              title="Next Theme"
+              on:click={() => theme = DCT.activateNextTheme()}>
+          {theme}
+        </span>
 
-          <span class="NON-SELECTED-COLOR text-onLight
-                       HOVER              hover:bg-primary hover:text-onDark
-                       CURSOR             select-none cursor-pointer
-                       OTHER              mr-1 px-3 py-2 rounded-md text-sm font-medium"
-                title="Toggle Dark/Light"
-                on:click={() => inversion = DCT.toggleInvertShade()}>
-            {inversion ? 'Dark' : 'Light'}
-          </span>
+        <span class="NON-SELECTED-COLOR text-onLight
+                     HOVER              hover:bg-primary hover:text-onDark
+                     CURSOR             select-none cursor-pointer
+                     OTHER              mr-1 px-3 py-2 rounded-md text-sm font-medium"
+              title="Toggle Dark/Light"
+              on:click={() => inversion = DCT.toggleInvertShade()}>
+          {inversion ? 'Dark' : 'Light'}
+        </span>
 
-          <Icon name="notifications"
-                class="PRIMARY-COLOR text-primary
-                       HOVER-COLOR   hover:text-primaryDark
-                       CURSOR        select-none cursor-pointer"/>
+        <Icon name="notifications"
+              class="PRIMARY-COLOR text-primary
+                     HOVER-COLOR   hover:text-primaryDark
+                     CURSOR        select-none cursor-pointer"/>
 
-          <!-- Profile dropdown -->
-          <div class="ml-3 relative">
-            <button class="bg-secondary
-                           flex text-sm rounded-full
-                           focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-secondaryDark focus:ring-secondaryLight"
-                    id="user-menu">
-              <Icon name="person"
-                    class="PRIMARY-COLOR text-primary
-                           HOVER-COLOR   hover:text-primaryDark
-                           CURSOR        select-none cursor-pointer"/>
-            </button>
-            <!--
-                 Profile dropdown panel, show/hide based on dropdown state.
+        <!-- Profile dropdown -->
+        <div class="ml-3 relative">
+          <button class="bg-secondary
+                         flex text-sm rounded-full
+                         focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-secondaryDark focus:ring-secondaryLight"
+                  id="user-menu">
+            <Icon name="person"
+                  class="PRIMARY-COLOR text-primary
+                         HOVER-COLOR   hover:text-primaryDark
+                         CURSOR        select-none cursor-pointer"/>
+          </button>
+          <!--
+               Profile dropdown panel, show/hide based on dropdown state.
 
-                 Entering: "transition ease-out    duration-200"
-                 From:     "transform  opacity-0   scale-95"
-                 To:       "transform  opacity-100 scale-100"
-                 Leaving:  "transition ease-in     duration-75"
-                 From:     "transform  opacity-100 scale-100"
-                 To:       "transform  opacity-0   scale-95"
-               -->
-            <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1
-                        bg-primaryLight
-                        ring-2 ring-primary ring-opacity-5
-                        CONTENT-RELATED text-sm font-bold select-none cursor-pointer"
-                 role="menu"
-                 aria-orientation="vertical"
-                 aria-labelledby="user-menu">
-              <span class="block px-4 py-2 text-primaryDark hover:bg-primary" role="menuitem">Your Profile</span>
-              <span class="block px-4 py-2 text-primaryDark hover:bg-primary" role="menuitem">Settings</span>
-              <span class="block px-4 py-2 text-primaryDark hover:bg-primary" role="menuitem">Sign Out</span>
-            </div>
+               Entering: "transition ease-out    duration-200"
+               From:     "transform  opacity-0   scale-95"
+               To:       "transform  opacity-100 scale-100"
+               Leaving:  "transition ease-in     duration-75"
+               From:     "transform  opacity-100 scale-100"
+               To:       "transform  opacity-0   scale-95"
+             -->
+          <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1
+                      bg-primaryLight
+                      ring-2 ring-primary ring-opacity-5
+                      CONTENT-RELATED text-sm font-bold select-none cursor-pointer"
+               role="menu"
+               aria-orientation="vertical"
+               aria-labelledby="user-menu">
+            <span class="block px-4 py-2 text-primaryDark hover:bg-primary" role="menuitem">Your Profile</span>
+            <span class="block px-4 py-2 text-primaryDark hover:bg-primary" role="menuitem">Settings</span>
+            <span class="block px-4 py-2 text-primaryDark hover:bg-primary" role="menuitem">Sign Out</span>
           </div>
         </div>
-      </div> <!-- end of ... Right Section of NavBar -->
+      </div>
+    </div> <!-- end of ... Right Section of NavBar -->
 
-    </div>
   </div>
 </nav>
 
